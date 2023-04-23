4da6c0b48097804a119a1faf0b1eecdb66ed3591
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2023-04-23 15:17:56 +0430

    js

--- js14.js
+++ js14.js
@@ -10,3 +10,20 @@ for (mabda;shart;code ezafi) {
 for(let num = 0; num < 10; num++){
      console.log(num);
 }
+
+// برای دیتا های قابل شمارش میتوان از حلقه for of استفاد کرد
+// دیتا های قابل شمارش = آرایه و استرینگ و شبه آرایه ها
+
+let name = 'mohammad'
+for (s of name){
+     console.log(s)
+}
+// برای دیتا های غیر قابل شمارش مانند آبجکت ها
+let obj = {
+     name:'mohammad',
+     age:18
+}
+
+for (o in obj){
+     console.log(o)
+}
--- js5.js
+++ js5.js
@@ -9,4 +9,11 @@ console.log(Name + "\n" + family); // mohammad
                                    // karimpour
                                    
 
-console.log("mohammad\"karimpour\""); // mohammad "karimpour"                                   
+console.log("mohammad\"karimpour\""); // mohammad "karimpour"
+
+//الان بهتر است از بک تیک برای ساخت استرینگ استفاده کرد
+
+let STR = `mohammad
+karimpour${Name}` //mohammad
+                  //karimpourmohammad
+console.log(STR)
