bd1466797b2caaad5f734c2fbc8119e18adff8b7
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2023-04-27 21:28:33 +0430

    js

---
+++ js53.js
@@ -0,0 +1,46 @@
+// ست و مپ یک نوع داده هستند که مثل آرایه و آبجکت میباشند
+// ست
+let SET = new Set()
+
+// برای اضافه کردن آیتم
+SET.add(12)
+// برای حذف آیتم
+SET.delete(12)
+// باری حذف کل آیتم ها
+SET.clear()
+// باری گرفتم اندازه ست
+SET.size()
+// برای جستجو یک آیتم
+SET.has(12)
+
+// ذخیره کردن یک آرایه به عنوان ست توی یک متغیر
+let num = [1,2,3,4,5,6,7,8,9]
+let NewSet = new Set(num)
+
+// ذخیره کردن یک ست به عنوان آرایه توی یک متغیر
+let NetArray = [...SET]
+
+
+
+//مپ
+//مپ مثل آبجکت است و کلید و ولیو دارد
+
+let MAP = new Map()
+
+// برای اضافه کردن آیتم
+MAP.set('number', 12)
+// برای حذف آیتم
+MAP.delete('number')
+// باری حذف کل آیتم ها
+MAP.clear()
+// باری گرفتم اندازه ست
+MAP.size()
+// برای جستجو یک آیتم
+MAP.has('number')
+// گرفتن یک آیتم
+MAP.get('number')
+
+// تبدیل کردن یک آبجکت به مپ
+let NewMap = new MAP(Object.entries({'name': 'mohammad'}))
+// تبدیل مپ به آبجکت
+let NewObj = Object.fromEntries(MAP)
