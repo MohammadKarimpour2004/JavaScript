b9d4776f2960cef5025cf45404c5ad2580ac1a71
mohammad karimpour <mohammad.karimpour1383@gmail.com>
2023-07-22 13:08:01 +0430

    js

--- js18.js
+++ js18.js
@@ -23,3 +23,15 @@ delete objects.family
 // آبجکت ها یک دیتا غیر قابل پیمایش هستند و برای قابل پیمایش شدنش باید
 let onPaymayesh = Object.entries(objects)
 console.log(onPaymayesh) // [ [ 'name', 'mohammad' ], [ 'age', 18 ], [ 'live', true ] ]
+
+
+let one = 1
+let two = 2
+let three = 3
+
+// اگر میخواهید مقداری را در آبجکت ذخیره کنید که از جایی دیگر می آید فقط لازم است آن را صدا کردن
+let nums ={
+    one,
+    two,
+    three
+} 
